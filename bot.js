56cdd8fd0cdc91ffb28294f7c4ba947967418be7
h4rdc0r3d <49732392+h4rdc0r3d@users.noreply.github.com>
2019-07-01 16:14:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,10 +45,13 @@ bot.on ('message', function(message){
     {
         message.channel.sendMessage(message.author + ' наверное **Орда**');
     }    
-   else if (message.content === `"паладин"`) {      
-    message.channel.type === (`"dm"`) + message.author.sendMessage(`"https://www.youtube.com/watch?v=pbkHJpYCl80"`) 
-    }
 });
+
+bot.on('message', msg => {    
+  if (msg.content === `"пал"`) {      
+    msg.channel.type === (`"dm"`) + msg.author.sendMessage(`"https://www.youtube.com/watch?v=pbkHJpYCl80"`) 
+  }
+}
 
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
     // Here I'm storing the IDs of their voice channels, if available
